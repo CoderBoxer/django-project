b296a71294fdd765006b11a7023d745e3f5b8876
CoderBoxer <boxercoder@gmail.com>
2025-05-02 22:45:13 +0600

    Done practicing

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('function', views.hello_world),
     path('class', views.HelloBangladesh.as_view()), # As_view() is used to convert class-based view to function-based view  
-
+    path('reservation', views.home), # This is the URL for the home page where the form is displayed
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .forms import ReservationForm
 
 # Create your views here.
 def hello_world(request):
@@ -9,3 +10,15 @@ def hello_world(request):
 class HelloBangladesh(View):
     def get(self, request):
         return HttpResponse("Hello, Bangladesh!")
+    
+def home(request):
+    form = ReservationForm()
+
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Reservation made successfully!")
+        
+    return render(request, 'index.html', {'form': form})
+
